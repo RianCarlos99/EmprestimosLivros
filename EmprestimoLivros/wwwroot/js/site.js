4774f5efd9f4f99a75af8e4cab6a629d4e80a069
eststitardedes <eststitardedes@STI-ESTAGIARIO>
2023-07-03 16:40:03 -0300

    logica do alert e a tradução da biblioteca datatables adicionado

--- EmprestimoLivros/wwwroot/js/site.js
+++ EmprestimoLivros/wwwroot/js/site.js
@@ -1,4 +1,36 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $('#Emprestimos').DataTable({
+        language: {
+            "decimal": "",
+            "emptyTable": "No data available in table",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ entradas",
+            "infoEmpty": "Showing 0 to 0 of 0 entries",
+            "infoFiltered": "(filtrado de _MAX_ total entradas)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ entradas",
+            "loadingRecords": "Carregando...",
+            "processing": "",
+            "search": "Procure:",
+            "zeroRecords": "Sem registros encontrados",
+            "paginate": {
+                "first": "Primeiro",
+                "last": "Último",
+                "next": "Próximo",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "sortAscending": ": activate to sort column ascending",
+                "sortDescending": ": activate to sort column descending"
+            }
+        },
+        //scrollY: 300,
+        "lengthMenu": [5, 10, 25, 50],
+    });
+});
 
-// Write your JavaScript code.
+setTimeout(() => {
+    $(".alert").fadeOut("slow", () => {
+        $(this).alert("close");
+    }) 
+}, 4000)
