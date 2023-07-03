e838bfa92f84a5ba4c3240c8630ea1e3031189ec
eststitardedes <eststitardedes@STI-ESTAGIARIO>
2023-07-03 16:28:00 -0300

    adicionado o alert pelo TempData

--- EmprestimoLivros/Controllers/EmprestimoController.cs
+++ EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -32,10 +32,81 @@ namespace EmprestimoLivros.Controllers
                 _db.Emprestimos.Add(emprestimo);
                 _db.SaveChanges();
 
+                TempData["MensagemSucesso"] = "Cadastro realizado com sucesso!";
+
                 return RedirectToAction("Index");
             }
 
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {   
+            if(id == null || id== 0)
+            {
+                return NotFound();
+            }
+
+            EmprestimosModel? emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+            
+            if(emprestimo == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(EmprestimosModel emprestimo)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Emprestimos.Update(emprestimo);
+                _db.SaveChanges();
+                
+                TempData["MensagemSucesso"] = "Cadastro editado com sucesso!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(emprestimo);
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            EmprestimosModel ?emprestimo = _db.Emprestimos.FirstOrDefault(e => e.Id == id);
+
+            if(emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(EmprestimosModel emprestimo)
+        {
+            if(emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            _db.Emprestimos.Remove(emprestimo);
+            _db.SaveChanges();
+
+            TempData["MensagemSucesso"] = "Cadastro excluido com sucesso!";
+
+            return RedirectToAction("Index");
         }
     }
 }
